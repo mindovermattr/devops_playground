2a3d135cbd8daed72fde57b283be703035fb161e
Дмитрий Беликов <dduim@bk.ru>
2026-07-15 04:15:05 +0300

    +docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const test = async () => {
+      const rest = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1",
+      );
+      console.log(rest.data);
+      return rest.data;
+    };
+
+    test();
+  }, []);
 
   return (
     <>
@@ -16,7 +29,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Get фывфыв</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
@@ -116,7 +129,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
